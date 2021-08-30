69cc40fd20321d88713ac8c9ad5bc965924e1ca1
allilk <milk@cream.glass>
2021-08-30 15:04:24 -0400

    prep for public

--- server.js
+++ server.js
@@ -1,54 +1,63 @@
-let express = require("express"),
-  app = express(),
-  port = process.env.PORT || 3000,
-  User = require("./models/user"),
-  Recipe = require("./models/recipe"),
-  bodyParser = require("body-parser"),
-  jsonwebtoken = require("jsonwebtoken"),
-  cors = require("cors");
+const express = require("express"),
+	app = express(),
+	mongoose = require("mongoose"),
+	bodyParser = require("body-parser"),
+	port = process.env.PORT || 3000;
+let jsonWebToken = require("jsonwebtoken"),
+	cors = require("cors"),
+	rateLimit = require("express-rate-limit");
+let User = require("./models/user"),
+	Recipe = require("./models/recipe"),
+	userRoutes = require("./routes/user"),
+	recipeRoutes = require("./routes/recipe");
 
-app.use(cors());
-const mongoose = require("mongoose");
 const option = {
-  socketTimeoutMS: 30000,
-  keepAlive: true,
-  reconnectTries: 30000,
-  useNewUrlParser: true,
+	socketTimeoutMS: 30000,
+	keepAlive: true,
+	reconnectTries: 30000,
+	useNewUrlParser: true,
 };
 
-// const mongoURI = process.env.MONGODB_URI;
-mongoose.connect('', option)
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+});
+
+mongoose.connect(
+	"",
+	option
+);
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-app.use(function (req, res, next) {
-  if (
-    req.headers &&
-    req.headers.authorization &&
-    req.headers.authorization.split(" ")[0] === "JWT"
-  ) {
-    jsonwebtoken.verify(
-      req.headers.authorization.split(" ")[1],
-      "RESTFULAPIs",
-      function (err, decode) {
-        if (err) req.user = undefined;
-        req.user = decode;
-        next();
-      }
-    );
-  } else {
-    req.user = undefined;
-    next();
-  }
+app.use(cors());
+app.use(limiter);
+app.use((req, res, next) => {
+	if (
+		req.headers &&
+		req.headers.authorization &&
+		req.headers.authorization.split(" ")[0] === "JWT"
+	) {
+		jsonWebToken.verify(
+			req.headers.authorization.split(" ")[1],
+			"RESTFULAPIs",
+			function (err, decode) {
+				if (err) req.user = undefined;
+				req.user = decode;
+				next();
+			}
+		);
+	} else {
+		req.user = undefined;
+		next();
+	}
 });
-let routes = require("./routes/user");
-routes(app);
-let recipeRoutes = require("./routes/recipe");
 
+userRoutes(app);
 recipeRoutes(app);
-app.use(function (req, res) {
-  res.status(404).send({ url: req.originalUrl + " not found" });
+app.use((req, res) => {
+	res.status(404).send({ url: req.originalUrl + " not found" });
 });
 
 app.listen(port);
